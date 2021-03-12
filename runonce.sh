4ac04f15c15de7e4a3f894ccdc6d86ad7b2b3c9f
Matheus Lozano <lozanomatheus@gmail.com>
2021-03-12 22:26:54 +0100

    Change the scope of the variable PODS to local Declare the variable DAEMONSET_NAME and move the PODS variable to the main Function

--- runonce.sh
+++ runonce.sh
@@ -7,18 +7,21 @@ set -euo pipefail
 function main() {
   kubectl apply -f configmap.yaml
   kubectl apply -f daemonset.yaml
-  wait_for_pods ssm-agent-installer
-  wait_for_success ssm-agent-installer
+
+  local -r DAEMONSET_NAME="ssm-agent-installer"
+  local -r PODS="$(kubectl get pods | grep -i "${DAEMONSET_NAME}" | awk '{print $1}')"
+  
+  wait_for_pods
+  wait_for_success
+
   kubectl delete -f daemonset.yaml
 }
 
 function wait_for_pods() {
-  echo -n "waiting for $1 pods to run"
-
-  PODS=$(kubectl get pods | grep $1 | awk '{print $1}')
+  echo -n "waiting for ${DAEMONSET_NAME} pods to run"
 
   for POD in ${PODS}; do
-    while [[ $(kubectl get pod ${POD} -o go-template --template "{{.status.phase}}") != "Running" ]]; do
+    while [[ "$(kubectl get pod "${POD}" -o go-template --template "{{.status.phase}}")" != "Running" ]]; do
       sleep 1
       echo -n "."
     done
@@ -28,12 +31,10 @@ function wait_for_pods() {
 }
 
 function wait_for_success() {
-  echo -n "waiting for $1 daemonset to complete"
-
-  PODS=$(kubectl get pods | grep $1 | awk '{print $1}')
+  echo -n "waiting for ${DAEMONSET_NAME} daemonset to complete"
 
   for POD in ${PODS}; do
-    while [[ $(kubectl logs ${POD} --tail 1) != "Success" ]]; do
+    while [[ $(kubectl logs "${POD}" --tail 1) != "Success" ]]; do
       sleep 1
       echo -n "."
     done
